ff46c363fc7e76d30cb668b86dcaffa693eb4372
nadeen-elsayed <dodynadeen0@gmail.com>
2024-01-26 16:26:36 -0500

    Code for regestering a new user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,33 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const doesExist = (username)=>{
+    let userswithsamename = users.filter((user)=>{
+      return user.username === username
+    });
+    if(userswithsamename.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) { 
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: `User ${username} successfully registred. Now you can login`});
+    } else {
+      return res.status(404).json({message: "User already exists!"});    
+    }
+  } 
+ 
+  return res.status(404).json({message: "Unable to register user."});
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
